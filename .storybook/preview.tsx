439ed73e6d328d07c26aff20af5eb5e74b749bfb
Stephane P. Pericat <shortwavaudio@gmail.com>
2024-06-07 21:47:49 -0500

    storybook

---
+++ .storybook/preview.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import type { Preview } from '@storybook/react'
+import { Inter as FontSans } from 'next/font/google'
+import { cn } from '../src/lib/utils'
+import '../src/app/globals.css'
+
+const fontSans = FontSans({
+  subsets: ['latin'],
+  variable: '--font-sans',
+})
+
+const preview: Preview = {
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+  },
+  decorators: [
+    (Story) => (
+      <div
+        className={cn(
+          'min-h-screen bg-background font-sans antialiased',
+          fontSans.variable
+        )}
+      >
+        <Story />
+      </div>
+    ),
+  ],
+}
+
+export default preview
